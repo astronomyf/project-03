f5ce0496f7410cb6cff865cfe216d3129f5ce9e1
Francesco Violante <francesco.violante96@gmail.com>
2020-09-16 19:29:37 +0200

    Nuove modifiche

---
+++ scripts/api.js
@@ -0,0 +1,42 @@
+/**
+ * @author Francesco Violante
+ * @file api.js
+ * 
+ * Api functions.
+ */
+
+ let stations = [];
+
+ const getData = async () => {
+
+    const response = await fetch("https://www.torinometeo.org/api/v1/realtime/data/", {
+        header: {
+            "Content-Type": "application/json" 
+        }
+    });
+
+    if(response.ok) {
+        const data = await response.json();
+        
+        for(const country of data) {
+            stations.push(country);
+        }
+
+        return data;
+
+    } else {
+        console.error("Data could not be obtained.", response.status);
+        
+        return null;
+    }
+
+ }
+
+ getData().then(data => {
+     // 
+     
+ })
+
+
+
+                
