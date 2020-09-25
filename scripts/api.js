6cdf0a808f0d1d89fa1b0c4c5d5a45af70f89c7f
Francesco Violante <francesco.violante96@gmail.com>
2020-09-25 14:48:24 +0200

    update

--- scripts/api.js
+++ scripts/api.js
@@ -18,24 +18,26 @@
     if(response.ok) {
         const data = await response.json();
         
-        for(const res of data) {
+        // for(const res of data) {
 
-             const station = res.station;
-             stations.push(new Station(station.id, 
-                                        station.city, 
-                                        station.description, 
-                                        station.nation.name, 
-                                        station.nation.alpha2_code, 
-                                        station.region.name, 
-                                        station.province.name, 
-                                        station.lat, station.lon, 
-                                        station.elevation, 
-                                        station.image_url, 
-                                        station.webcam, 
-                                        res.weather_icon.icon, 
-                                        res.temperature, 
-                                        res.relative_humidity));
-         }
+        //      const station = res.station;
+        //      stations.push(new Station(station.id, 
+        //                                 station.city, 
+        //                                 station.description, 
+        //                                 station.nation.name, 
+        //                                 station.nation.alpha2_code, 
+        //                                 station.region.name, 
+        //                                 station.province.name, 
+        //                                 station.lat, station.lon, 
+        //                                 station.elevation, 
+        //                                 station.image_url, 
+        //                                 station.webcam, 
+        //                                 res.weather_icon.icon, 
+        //                                 res.temperature, 
+        //                                 res.relative_humidity));
+        //  }
+
+        return data;
 
     } else {
         console.error("Data could not be obtained.", response.status);
